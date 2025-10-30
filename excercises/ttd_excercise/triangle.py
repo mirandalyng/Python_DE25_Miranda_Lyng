d607efec33386e8843249066e5acd27d76fb6901
Miranda Lyng <lyngmiranda@gmail.com>
2025-10-30 10:48:21 +0100

    ttd tests

---
+++ excercises/ttd_excercise/triangle.py
@@ -0,0 +1,50 @@
+
+#Skeleton for the Triangle class 
+class Triangle: 
+    #x and y optional 
+    def __init__(self, base, height):
+        pass
+
+    
+    @property
+    def base(self): 
+        pass
+
+    @base.setter
+    def base(self): 
+         pass 
+
+    @property
+    def height(self): 
+        pass 
+
+    @height.setter
+    def height(self): 
+         pass
+
+
+    @property
+    def area(self): 
+        pass 
+
+    # @property
+    # def perimeter(self): 
+    #     pass
+
+
+    def __eq__(self, other): 
+        pass
+
+    def __lt__(self, other): 
+        pass
+
+    
+    def __gt__(self, other): 
+        pass
+
+
+    def __repr__(self): 
+        pass 
+
+    
+
